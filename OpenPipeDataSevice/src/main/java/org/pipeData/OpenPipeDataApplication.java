be7d17ac739ee140d3ef3c759c1ae1ab5dcfaac3
L\zhang <867997639@qq.com>
2023-10-30 14:39:59 +0800

    添加运行服务打开浏览器的功能

--- OpenPipeDataSevice/src/main/java/org/pipeData/OpenPipeDataApplication.java
+++ OpenPipeDataSevice/src/main/java/org/pipeData/OpenPipeDataApplication.java
@@ -1,19 +1,62 @@
 package org.pipeData;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.pipeData.config.condition.OpenBrowserCondition;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
 
 @Configuration
 @EnableWebMvc
 @SpringBootApplication(scanBasePackages = {"org.pipeData"})
-public class OpenPipeDataApplication {
+public class OpenPipeDataApplication implements ApplicationListener<ContextRefreshedEvent> {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(OpenPipeDataApplication.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        openBrowser();
+    }
+
+    private void openBrowser() {
+        String url = "http://localhost:" + serverPort+"/swagger-ui/index.html";
+        String os = System.getProperty("os.name").toLowerCase();
+
+        try {
+            if (os.contains("win")) {
+                // Windows
+                Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } else if (os.contains("mac")) {
+                // Mac OS
+                Runtime.getRuntime().exec("open " + url);
+            } else if (os.contains("nix") || os.contains("nux") || os.contains("bsd")) {
+                // Linux or Unix
+                Runtime.getRuntime().exec("xdg-open " + url);
+            }
+        } catch (IOException e) {
+            // 处理异常
+        }
     }
 
 }
--- OpenPipeDataSevice/src/main/java/org/pipeData/config/condition/OpenBrowserCondition.java
+++ OpenPipeDataSevice/src/main/java/org/pipeData/config/condition/OpenBrowserCondition.java
@@ -1,2 +1,17 @@
-package org.pipeData.config.condition;public class OpenBrowserCondition {
+package org.pipeData.config.condition;
+
+import org.springframework.boot.autoconfigure.condition.AnyNestedCondition;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+
+public class OpenBrowserCondition  extends AnyNestedCondition {
+    public OpenBrowserCondition(ConfigurationPhase configurationPhase) {
+        super(configurationPhase);
+    }
+
+    @ConditionalOnProperty(value = "open.browser.enabled", havingValue = "true")
+    static class EnableOpenBrowser {}
+
+    @ConditionalOnProperty(value = "open.browser.enabled", havingValue = "false", matchIfMissing = true)
+    static class DisableOpenBrowser {}
+
 }
