89ea375a812d3c60f02ba67b515bcdb5a8f9c117
WIN-OUR7VTUTUJ8\Administrator <867997639@qq.com>
2023-11-02 17:32:16 +0800

    增加用户登录接口

---
+++ OpenPipeDataSevice/src/main/java/org/pipeData/controller/UserController.java
@@ -0,0 +1,15 @@
+package org.pipeData.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserController  extends BaseController{
+
+//    public ResponseData
+
+
+
+}
---
+++ OpenPipeDataSevice/src/main/java/org/pipeData/core/entity/UserBaseInfo.java
@@ -0,0 +1,10 @@
+package org.pipeData.core.entity;
+
+
+import lombok.Data;
+
+@Data
+public class UserBaseInfo {
+
+
+}
