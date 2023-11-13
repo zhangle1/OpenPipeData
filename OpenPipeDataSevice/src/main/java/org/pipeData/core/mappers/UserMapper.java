77e3c09b034235cdba5833ac617c92f7a523f1e6
WIN-OUR7VTUTUJ8\Administrator <867997639@qq.com>
2023-11-13 14:49:04 +0800

    增加repository,以及  生产模式和dev模式判断

--- OpenPipeDataSevice/src/main/java/org/pipeData/core/mappers/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package org.pipeData.core.mappers;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.pipeData.core.entity.User;
-
-
-
-public interface UserMapper extends BaseMapper<User> {
-}
